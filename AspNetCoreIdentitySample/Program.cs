61752152a983f8e7d5d55c0dd204346672f485fa
berkaysezeer <berkaysezeer@hotmail.com>
2020-06-10 21:30:39 +0300

    identity scaffold & program.cs seed

--- AspNetCoreIdentitySample/Program.cs
+++ AspNetCoreIdentitySample/Program.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreIdentitySample.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,9 +14,48 @@ namespace AspNetCoreIdentitySample
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public async static Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build(); //yapici
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                #region þehirler
+                var db = services.GetRequiredService<ApplicationDbContext>();
+
+                if (!db.Sehirler.Any())
+                {
+                    db.Sehirler.Add(new Sehir { Id = 1, sehirAd = "Konya" });
+                    db.Sehirler.Add(new Sehir { Id = 2, sehirAd = "Balýkesir" });
+                    db.Sehirler.Add(new Sehir { Id = 3, sehirAd = "Ankara" });
+
+                    db.SaveChanges();
+                }
+                #endregion
+
+                #region roles
+                var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                await roleManager.CreateAsync(new IdentityRole { Name = "admin" });
+                #endregion
+
+                #region users
+                var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+
+                var adminUser = new IdentityUser { UserName = "admin@example.com", Email = "admin@example.com", EmailConfirmed = true };
+
+                await userManager.CreateAsync(adminUser, "Password1.");
+                adminUser = await userManager.FindByNameAsync("admin@example.com");
+                await userManager.AddToRoleAsync(adminUser, "admin");
+
+                var sampleUser = new IdentityUser { UserName = "user@example.com", Email = "user@example.com", EmailConfirmed = true };
+                await userManager.CreateAsync(sampleUser, "Password1.");
+
+
+
+                #endregion
+            }
+            host.Run(); //calistir
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
